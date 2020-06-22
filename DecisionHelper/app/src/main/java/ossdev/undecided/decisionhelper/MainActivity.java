4bcfe0e2712f56602b49eaf16bd972a888b628fe
권태윤 <kwontaeyoon@gwontaeyun-ui-MacBookPro.local>
2020-06-22 16:17:33 +0900

    add login & register

--- DecisionHelper/app/src/main/java/ossdev/undecided/decisionhelper/MainActivity.java
+++ DecisionHelper/app/src/main/java/ossdev/undecided/decisionhelper/MainActivity.java
@@ -2,13 +2,28 @@ package ossdev.undecided.decisionhelper;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView tv_id, tv_pass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tv_id = findViewById(R.id.tv_id);
+        tv_pass = findViewById(R.id.tv_pass);
+
+        Intent intent = getIntent();
+        String userID = intent.getStringExtra("userID");
+        String userPass = intent.getStringExtra("userPass");
+
+        tv_id.setText(userID);
+        tv_pass.setText(userPass);
+
     }
 }
